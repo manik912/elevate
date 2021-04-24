90fc10c7c4140d713c0247738a9c22307b28b39a
N15H4N7 <62706874+N15H4N7@users.noreply.github.com>
2021-04-24 18:24:54 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -4,19 +4,19 @@ from .views import *
 
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('buy/', buyMaterial, name='buy'),\
-    path('manufacture/', manufacture, name='manufacture'),
-    path('trade/', send_req, name='trade'),
-    path('accept/<int:pk>/', accept_req, name='accept'),
-    path('reject/<int:pk>/', reject_req, name='reject'),
-    path('delete/<int:pk>/', delete_req, name='delete'),
-    path('test/', test, name='test'),
-    path('get-req/ajax/', pending_req, name='get-req'),
-    path('get-quan/ajax/', get_quantity, name='get-quan'),
-    path('buy/ajax/', get_rmc, name='get-rmc'),
-    path('sellus/', sell_us, name='sellus'),
-    path('category/', cat, name='category'),
-    path('instruction/', instruction, name='instruction'),
+#     path('', home, name='home'),
+#     path('buy/', buyMaterial, name='buy'),\
+#     path('manufacture/', manufacture, name='manufacture'),
+#     path('trade/', send_req, name='trade'),
+#     path('accept/<int:pk>/', accept_req, name='accept'),
+#     path('reject/<int:pk>/', reject_req, name='reject'),
+#     path('delete/<int:pk>/', delete_req, name='delete'),
+#     path('test/', test, name='test'),
+#     path('get-req/ajax/', pending_req, name='get-req'),
+#     path('get-quan/ajax/', get_quantity, name='get-quan'),
+#     path('buy/ajax/', get_rmc, name='get-rmc'),
+#     path('sellus/', sell_us, name='sellus'),
+#     path('category/', cat, name='category'),
+#     path('instruction/', instruction, name='instruction'),
 ]
 
